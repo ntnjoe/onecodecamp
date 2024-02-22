923e0be44f4d29233a8cd025d6d780e44693f0ba
ntnjoe <jnatan0203@gmail.com>
2024-02-22 15:21:46 +0800

    Created the activity 1 using react on App.js

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [time, setTime] = useState(new Date().toLocaleTimeString());
+	const [time2, setTime2] = useState(new Date().toLocaleTimeString());
+
+	useEffect(() => {
+		setInterval(() => {
+			setTime2(new Date().toLocaleTimeString());
+		}, 1000);
+	}, []);
+
+	return (
+		<div className="App">
+			<div id="act1">
+				<h1>Hello World</h1>
+				<h2>It is{time}</h2>
+			</div>
+			<div id="act2">
+				<h1>Hello World</h1>
+				<h2>It is {time2}</h2>
+			</div>
+		</div>
+	);
 }
 
 export default App;
